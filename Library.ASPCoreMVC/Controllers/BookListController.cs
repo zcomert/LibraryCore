3d37e46b262f55c3ba1c90b00234cfc02aa9f6ea
Zafer CÖMERT <zcomert@samsun.edu.tr>
2020-12-19 22:54:00 +0300

    MVC Project has been added.

---
+++ Library.ASPCoreMVC/Controllers/BookListController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.Business.DependencyResolvers.Ninject;
+using Library.Business.Abstract;
+
+namespace Library.ASPCoreMVC.Controllers
+{
+    public class BookListController : Controller
+    {
+        public IActionResult Index()
+        {
+            var bookService = InstanceFactory.GetInstance<IBookService>();
+            var list = bookService.GetAll();
+            return View(list);
+        }
+    }
+}
---
+++ Library.ASPCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Library.ASPCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
